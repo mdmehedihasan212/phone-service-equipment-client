bf59bbebe35359961681a9af996ee4674949bc96
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-05-01 04:41:43 +0600

    add delete fetch method

--- src/Pages/Home/ManageInventory/ManageInventory.js
+++ src/Pages/Home/ManageInventory/ManageInventory.js
@@ -3,7 +3,24 @@ import useProduct from '../../../Hooks/useProduct';
 import { MdDeleteForever } from 'react-icons/md';
 
 const ManageInventory = () => {
-    const [products] = useProduct();
+    const [products, setProducts] = useProduct();
+
+    const handleToDelete = id => {
+        // console.log('clicked', id);
+        const proceed = window.confirm('Are you sure delete item!')
+        if (proceed) {
+            const url = `https://fathomless-hamlet-80982.herokuapp.com/product/${id}`;
+            fetch(url, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    const reaming = products?.filter(product => product._id !== id)
+                    setProducts(reaming)
+                    console.log(data);
+                })
+        }
+    }
 
     return (
         <section>
@@ -17,7 +34,7 @@ const ManageInventory = () => {
                                     <td className='py-4'>{product.name}</td>
                                     <td className='py-4'>${product.price}</td>
                                     <td className='py-4'>
-                                        <MdDeleteForever color='red' fontSize={'1.5em'} />
+                                        <MdDeleteForever onClick={() => handleToDelete(product._id)} cursor={'pointer'} color='red' fontSize={'1.8em'} />
                                     </td>
                                 </tr>
                             </tbody>
